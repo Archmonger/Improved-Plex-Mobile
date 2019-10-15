540b5c9a5d754791c03ae9a45ade26405e2133a2
Archmonger <>
2019-10-15 14:06:15 -0700

    wait for head to exit before applying customizations

--- plex_mobile.js
+++ plex_mobile.js
@@ -1,18 +1,42 @@
-/* Set viewport scale to 1 */
-var meta = document.createElement('meta');
-meta.name = "viewport";
-meta.content = "width=device-width, initial-scale=1";
-document.getElementsByTagName('head')[0].appendChild(meta);
+function elementReady(selector) {
+	return new Promise((resolve, reject) => {
+		let el = document.querySelector(selector);
+		if (el) {
+			resolve(el);
+		}
+		new MutationObserver((mutationRecords, observer) => {
+				// Query for elements matching the specified selector
+				Array.from(document.querySelectorAll(selector)).forEach((element) => {
+					resolve(element);
+					// Once we have resolved we don't need the observer anymore.
+					observer.disconnect();
+				});
+			})
+			.observe(document.documentElement, {
+				childList: true,
+				subtree: true
+			});
+	});
+}
 
-/* Prevent play btn from double clicking */
-document.getElementById('plex').addEventListener('touchstart', (e) => {
-	let resumeButton = document.querySelectorAll('button[data-qa-id="resumeButton"]')[0];
-	let pauseButton = document.querySelectorAll('button[data-qa-id="pauseButton"]')[0];
+elementReady("head").then( // Email button
+	(elementsAreLoaded) => {
+		/* Set viewport scale to 1 */
+		var meta = document.createElement('meta');
+		meta.name = "viewport";
+		meta.content = "width=device-width, initial-scale=1";
+		document.getElementsByTagName('head')[0].appendChild(meta);
 
-	if (
-		resumeButton && resumeButton.contains(e.target) ||
-		pauseButton && pauseButton.contains(e.target)
-	) {
-		e.preventDefault();
-	}
-});
+		/* Prevent play btn from double clicking */
+		document.getElementById('plex').addEventListener('touchstart', (e) => {
+			let resumeButton = document.querySelectorAll('button[data-qa-id="resumeButton"]')[0];
+			let pauseButton = document.querySelectorAll('button[data-qa-id="pauseButton"]')[0];
+
+			if (
+				resumeButton && resumeButton.contains(e.target) ||
+				pauseButton && pauseButton.contains(e.target)
+			) {
+				e.preventDefault();
+			}
+		});
+	});
