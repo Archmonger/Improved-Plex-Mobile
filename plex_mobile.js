485efbdb7e84e055b46e4ce299bc5809c0d30735
Archmonger <>
2019-10-15 00:45:15 -0700

    fix #1

--- plex_mobile.js
+++ plex_mobile.js
@@ -1,4 +1,18 @@
+/* Set viewport scale to 1 */
 var meta = document.createElement('meta');
 meta.name = "viewport";
 meta.content = "width=device-width, initial-scale=1";
 document.getElementsByTagName('head')[0].appendChild(meta);
+
+/* Prevent play btn from double clicking */
+document.getElementById('plex').addEventListener('touchstart', (e) => {
+	let resumeButton = document.querySelectorAll('button[data-qa-id="resumeButton"]')[0];
+	let pauseButton = document.querySelectorAll('button[data-qa-id="pauseButton"]')[0];
+
+	if (
+		resumeButton && resumeButton.contains(e.target) ||
+		pauseButton && pauseButton.contains(e.target)
+	) {
+		e.preventDefault();
+	}
+});
